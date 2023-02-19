5491b0f63888fd4d957f85e48422bec6083273f1
jethins <jethins@rediffmail.com>
2023-02-19 01:44:59 -0700

    brackets

---
+++ src/main/java/com/greatlearning/BalancingBrackets.java
@@ -0,0 +1,60 @@
+package com.greatlearning;
+
+import java.util.Stack;
+
+public class BalancingBrackets {
+
+    static boolean checkingBracketsBalanced(String bracketExpression)
+    {
+
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < bracketExpression.length(); i++)
+        {
+            char character = bracketExpression.charAt(i);
+
+            if (character == '(' || character == '[' || character == '{')
+            {
+                stack.push(character);
+                continue;
+            }
+
+            if (stack.isEmpty())
+                return false;
+            char c;
+
+            switch (character) {
+
+                case '}':
+                    c = stack.pop();
+                    if (c == '(' || c == '[')
+                        return false;
+                    break;
+
+                case ')':
+                    c = stack.pop();
+                    if (c == '{' || c == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    c = stack.pop();
+                    if (c == '(' || c == '{')
+                        return false;
+                    break;
+            }
+        }
+        return (stack.isEmpty());
+    }
+
+    // Driver code
+    public static void main(String[] args)
+    {
+        String bracketExpression = "([[{}]])";
+        Boolean result;
+        result = checkingBracketsBalanced(bracketExpression);
+        if (result)
+            System.out.println("The entered String has balanced brackets.");
+        else
+            System.out.println("The entered Strings do not contain balanced brackets.");
+    }
+}
